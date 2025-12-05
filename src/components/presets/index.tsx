260f46b2e31f25e5a08a2f27470adaef6a977d49
Karelle Hofler <karellehofler@gmail.com>
2025-12-05 16:29:44 -0500

    Updated index.tsx

--- src/components/presets/index.tsx
+++ src/components/presets/index.tsx
@@ -1,5 +1,5 @@
 import SavePresetButton from './save-preset-button'
 import DeletePresetButton from './delete-preset-button'
-import LoadPresetButton from './LoadPresetButton'
+import LoadPresetButton from './load-preset-button'
 
 export { SavePresetButton, DeletePresetButton, LoadPresetButton }
