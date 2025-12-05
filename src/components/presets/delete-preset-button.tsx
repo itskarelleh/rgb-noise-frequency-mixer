f87f9757c7df2d602c6a0d2c007d8bebda7fa175
Karelle Hofler <karellehofler@gmail.com>
2025-12-05 16:21:27 -0500

    Updated delete-preset-button.tsx

--- src/components/presets/delete-preset-button.tsx
+++ src/components/presets/delete-preset-button.tsx
@@ -4,7 +4,7 @@ export function DeletePresetButton({ id } : { id: string }) {
 
   return (
       <button
-        onClick={() => deletePreset(preset.id)}
+        onClick={() => deletePreset(id)}
         className="absolute top-0 right-0 transform translate-x-1 -translate-y-1 bg-red-600 hover:bg-red-700 text-white p-1 rounded-full transition-colors"
       >
         <Trash2 size={12} />
