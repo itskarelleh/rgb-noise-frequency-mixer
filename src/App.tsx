c2a886efeacf7d32ffee1b6fb4f0b38dccb2b6a2
Karelle Hofler <karellehofler@gmail.com>
2025-12-05 16:20:03 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -575,12 +575,7 @@ export default function RGBNoiseMixer() {
                   title={preset.name}
                 />
                 {hoveredPresetId === preset.id && (
-                  <button
-                    onClick={() => deletePreset(preset.id)}
-                    className="absolute top-0 right-0 transform translate-x-1 -translate-y-1 bg-red-600 hover:bg-red-700 text-white p-1 rounded-full transition-colors"
-                  >
-                    <Trash2 size={12} />
-                  </button>
+                    <DeletePresetButton />
                 )}
               </div>
             ))}
