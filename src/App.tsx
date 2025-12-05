cc91cc739c77cd581385c36a2b58385167547808
Karelle Hofler <karellehofler@gmail.com>
2025-12-05 16:14:06 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -646,13 +646,7 @@ export default function RGBNoiseMixer() {
                 )}
               </div>
             ))}
-            <button
-              onClick={savePreset}
-              className="w-12 h-12 rounded-lg border-2 border-dashed border-green-500 hover:border-green-400 bg-green-600 bg-opacity-10 hover:bg-opacity-20 transition-all flex items-center justify-center"
-              title="Add new preset"
-            >
-              <Plus size={20} className="text-green-400" />
-            </button>
+            <SavePresetButton />
           </div>
         </div>
 
