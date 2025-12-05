7f6c4d24c6376759689e64ce23e53a50c52affc8
Karelle Hofler <karellehofler@gmail.com>
2025-12-05 16:27:08 -0500

    Updated load-preset-button.tsx

--- src/components/presets/load-preset-button.tsx
+++ src/components/presets/load-preset-button.tsx
@@ -6,10 +6,10 @@ export default function LoadPresetButton({ preset } : { preset: Preset}) {
   const { loadPreset } = usePresets();
   return (
     <button
-                  onClick={() => loadPreset(preset)}
-                  className="w-12 h-12 rounded-lg shadow-lg border-2 border-gray-600 hover:border-white transition-colors"
-                  style={{ backgroundColor: rgbToHex(preset.red, preset.green, preset.blue) }}
-                  title={preset.name}
-                />
+      onClick={() => loadPreset(preset)}
+      className="w-12 h-12 rounded-lg shadow-lg border-2 border-gray-600 hover:border-white transition-colors"
+      style={{ backgroundColor: rgbToHex(preset.red, preset.green, preset.blue) }}
+      title={preset.name}
+    />
   )
 }
