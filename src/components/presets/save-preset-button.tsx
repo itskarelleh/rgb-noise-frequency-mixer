31d11d6fe7e71183cae99c106c1b2da6f02c5396
Karelle Hofler <karellehofler@gmail.com>
2025-12-05 16:12:04 -0500

    Updated save-preset-button.tsx

---
+++ src/components/presets/save-preset-button.tsx
@@ -0,0 +1,12 @@
+export function SavePresetButton() {
+
+  return (
+    <button
+      onClick={savePreset}
+      className="w-12 h-12 rounded-lg border-2 border-dashed border-green-500 hover:border-green-400 bg-green-600 bg-opacity-10 hover:bg-opacity-20 transition-all flex items-center justify-center"
+      title="Add new preset"
+    >
+      <Plus size={20} className="text-green-400" />
+    </button>
+  )
+}
