cc7d09a2134abeabe7a6b8ac4fe313c6d03d5003
Karelle Hofler <karellehofler@gmail.com>
2025-12-05 16:16:54 -0500

    Updated index.ts

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+interface Preset {
+  id: string;
+  name: string;
+  red: number;
+  green: number;
+  blue: number;
+  isOrganic: boolean;
+  lfoRate: number;
+  lfoDepth: number;
+  lfoShape: string;
+  randomness: number;
+  phaseOffset: number;
+}
